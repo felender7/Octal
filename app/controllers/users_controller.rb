1f2422b3e993cc731301bd3258e8cd3c4ed55991
felender <felender7@gmail.com>
2020-08-28 05:26:41 +0200

    modified octal logo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
   before_action :display_user_content, only:[:show]
   def index
+   if user_signed_in?
     @users = User.all
+  end
   end
 
   def show
+   if user_signed_in?
     @user = User.friendly.find(params[:id])
+  end
   end
 
   def display_user_content
-      @user = User.friendly.find(params[:id])
-      @display_questions = @user.succours
+      if user_signed_in?
+        @user = User.friendly.find(params[:id])
+        @display_questions = @user.succours
+    end
   end
 end
